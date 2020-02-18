573f764f7818e60c11e9e90be118a42c797b2130
edypeira96 <61199625+edypeira96@users.noreply.github.com>
2020-02-18 08:30:04 -0800

    Add files via upload

---
+++ practica2.py
@@ -0,0 +1,7 @@
+#Esta practica nor sirve para entender ciclos 
+n = input("ingresa un numero entre 0 y 20 ") #variable n incializada como 5
+n=int(n)
+while n > 0:
+  print(n)
+  n = n - 1 
+print("final")  
