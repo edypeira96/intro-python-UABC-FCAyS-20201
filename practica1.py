d095ff3504daa8c67b0b0b6735602153058fdb12
edypeira96 <61199625+edypeira96@users.noreply.github.com>
2020-02-18 08:28:31 -0800

    Add files via upload

---
+++ practica1.py
@@ -0,0 +1,10 @@
+nombre = input("escribe tu nombre ")
+print("hola" +  nombre)
+  
+edad = input("cuantos años tienes")
+x = int (edad)
+if x < 18:
+   print("Es usted mayor de edad")
+else:
+    print("Es usted mayor de edad")
+print("¡Hasta la próxima!")
