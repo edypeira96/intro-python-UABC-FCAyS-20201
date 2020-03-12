dd8964d92888beef6844e0788b07ee37f8e6c370
edypeira96 <61199625+edypeira96@users.noreply.github.com>
2020-03-12 09:00:26 -0700

    Nueva practica enfocada en diagramas de flujo
    La practica 4 fue un sencillo algoritmo para suma numero acompañado de su respectivo diagrama de flujo

--- practica4.py
+++ practica4.py
@@ -1,32 +1,11 @@
-class Estudiante:
-  # Indicamos atributos para  la clase 
-  edad = 0
-  carrera = "Desconocida"
-  Universidad = "Desconocida"
-  Genero = "Femenino"
-  
-  #definimos funciones
-  def  festejar(selft) :
-    print("festejando")
-    
-  def estudiar(self,materia) :
-    print("Estudiando..."+ materia)
-    
-  def llorar(self) :
-    print("llorando")
-  
-  def dormir(self) :
-    print("durmiendo")
-    
-    #Ajustamos la edad
-  def cambiarEdad(self, edadAlumno) :
-    self.edad = edadAlumno
-    print("Nueva edad ", edadAlumno)
-    
-   #generamos un nuevo estudiante 
-Eduardo = Estudiante()
-Eduardo.estudiar("logica para progrmacion")
-# imprimimos atributo del objeto
-Eduardo.cambiarEdad(23)
-print(Eduardo.edad)
+# el siguiente algoritmo resulve el problema para realizar la suma de dos numeros decimales
+a = 0
+b = 0 
+c = 0 
 
+a =float(input("ingrese el primer numero" ))
+b = float(input("ingrese el segundo numero" ))
+
+c = a + b 
+
+print ("la suma es : " ,c)
